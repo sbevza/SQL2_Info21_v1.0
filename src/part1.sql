430475dff27d89c668bfe249b9800807bb027d8f
chuckmon <chuckmon@student.21-school.ru>
2023-09-11 01:14:28 +0300

    Add constraints. Fix Collumn names

--- src/part1.sql
+++ src/part1.sql
@@ -5,119 +5,147 @@ CREATE DATABASE school21;
 \c school21
 
 -- Создаем таблицу Peers
-CREATE TABLE Peers (
-  id SERIAL PRIMARY KEY,
-  username VARCHAR(50) NOT NULL,
-  birthday DATE NOT NULL
+CREATE TABLE IF NOT EXISTS Peers
+(
+    Nickname VARCHAR PRIMARY KEY NOT NULL,
+    Birthday DATE                NOT NULL
 );
 
 -- Создаем таблицу Tasks
-CREATE TABLE Tasks (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  parent_task VARCHAR(100),  
-  max_xp INT NOT NULL
+CREATE TABLE IF NOT EXISTS Tasks
+(
+    Title      VARCHAR PRIMARY KEY NOT NULL,
+    ParentTask VARCHAR,
+    MaxXP      INT                 NOT NULL,
+    CONSTRAINT fk_tasks_parent_task FOREIGN KEY (ParentTask) REFERENCES Tasks (Title),
+    CONSTRAINT unique_pair CHECK (ParentTask != Title)
 );
 
 -- Создаем тип enum для статуса проверки
-CREATE TYPE check_status AS ENUM ('Start', 'Success', 'Failure');
+DO
+$$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'check_status') THEN
+            CREATE TYPE check_status AS ENUM ('Start', 'Success', 'Failure');
+        END IF;
+    END
+$$;
+
+-- Создаем таблицу Checks
+CREATE TABLE Checks
+(
+    ID   SERIAL PRIMARY KEY,
+    Peer VARCHAR NOT NULL,
+    Task VARCHAR NOT NULL,
+    Date DATE    NOT NULL,
+    CONSTRAINT fk_checks_peer_nickname FOREIGN KEY (Peer) REFERENCES Peers (Nickname),
+    CONSTRAINT fk_checks_task_title FOREIGN KEY (Task) REFERENCES Tasks (Title)
+);
 
 -- Создаем таблицу P2P
-CREATE TABLE P2P (
-  id SERIAL PRIMARY KEY,
-  check_id INT NOT NULL,
-  peer_username VARCHAR(50) NOT NULL,
-  check_status check_status NOT NULL,
-  checked_at TIMESTAMP NOT NULL
+CREATE TABLE IF NOT EXISTS P2P
+(
+    ID           SERIAL PRIMARY KEY,
+    "Check"      INT          NOT NULL,
+    CheckingPeer VARCHAR      NOT NULL,
+    State        check_status NOT NULL,
+    Time         TIME         NOT NULL,
+    CONSTRAINT fk_p2p_check_id FOREIGN KEY ("Check") REFERENCES Checks (ID),
+    CONSTRAINT fk_p2p_checking_peer_nickname FOREIGN KEY (CheckingPeer) REFERENCES Peers (Nickname)
 );
 
 -- Создаем таблицу Verter 
-CREATE TABLE Verter (
-  id SERIAL PRIMARY KEY,
-  check_id INT NOT NULL,
-  check_status check_status NOT NULL,
-  checked_at TIMESTAMP NOT NULL
-); 
-
--- Создаем таблицу Checks
-CREATE TABLE Checks (
-  id SERIAL PRIMARY KEY,
-  peer_username VARCHAR(50) NOT NULL,
-  task_name VARCHAR(100) NOT NULL,
-  checked_at DATE NOT NULL
+CREATE TABLE IF NOT EXISTS Verter
+(
+    ID      SERIAL PRIMARY KEY,
+    "Check" BIGINT       NOT NULL,
+    State   check_status NOT NULL,
+    Time    TIME         NOT NULL,
+    CONSTRAINT fk_verter_check_id FOREIGN KEY ("Check") REFERENCES Checks (ID)
 );
 
 -- Создаем таблицу TransferredPoints
-CREATE TABLE TransferredPoints (
-  id SERIAL PRIMARY KEY,
-  from_peer VARCHAR(50) NOT NULL,
-  to_peer VARCHAR(50) NOT NULL, 
-  points_amount INT NOT NULL
+CREATE TABLE IF NOT EXISTS TransferredPoints
+(
+    ID           SERIAL PRIMARY KEY,
+    CheckingPeer VARCHAR NOT NULL,
+    CheckedPeer  VARCHAR NOT NULL,
+    PointsAmount INT     NOT NULL,
+    CONSTRAINT fk_transferred_points_checking_peer_nickname
+        FOREIGN KEY (CheckingPeer) REFERENCES Peers (Nickname),
+    CONSTRAINT fk_transferred_points_checked_peer_nickname
+        FOREIGN KEY (CheckedPeer) REFERENCES Peers (Nickname)
 );
 
 -- Создаем таблицу Friends
-CREATE TABLE Friends (
-  id SERIAL PRIMARY KEY,
-  peer1 VARCHAR(50) NOT NULL,
-  peer2 VARCHAR(50) NOT NULL
+CREATE TABLE Friends
+(
+    id    SERIAL PRIMARY KEY,
+    peer1 VARCHAR     NOT NULL,
+    peer2 VARCHAR(50) NOT NULL
 );
 
 -- Создаем таблицу Recommendations  
-CREATE TABLE Recommendations (
-  id SERIAL PRIMARY KEY,
-  peer VARCHAR(50) NOT NULL,
-  recommended_peer VARCHAR(50) NOT NULL
+CREATE TABLE Recommendations
+(
+    id               SERIAL PRIMARY KEY,
+    peer             VARCHAR(50) NOT NULL,
+    recommended_peer VARCHAR(50) NOT NULL
 );
 
 -- Создаем таблицу XP
-CREATE TABLE XP (
-  id SERIAL PRIMARY KEY,
-  check_id INT NOT NULL,
-  xp_amount INT NOT NULL 
+CREATE TABLE XP
+(
+    id        SERIAL PRIMARY KEY,
+    check_id  INT NOT NULL,
+    xp_amount INT NOT NULL
 );
 
 -- Создаем таблицу TimeTracking
-CREATE TABLE TimeTracking (
-  id SERIAL PRIMARY KEY,
-  peer VARCHAR(50) NOT NULL,
-  date DATE NOT NULL, 
-  time TIME NOT NULL,
-  state INT NOT NULL 
+CREATE TABLE TimeTracking
+(
+    id    SERIAL PRIMARY KEY,
+    peer  VARCHAR(50) NOT NULL,
+    date  DATE        NOT NULL,
+    time  TIME        NOT NULL,
+    state INT         NOT NULL
 );
 
 -----------------------------------------------------------
 
-  -- Процедура импорта данных в таблицу Peers
+-- Процедура импорта данных в таблицу Peers
 CREATE OR REPLACE FUNCTION import_from_csv(
-    filename text, 
+    filename text,
     delimiter char DEFAULT ','
 )
-RETURNS void
-LANGUAGE plpgsql
-AS $$
+    RETURNS void
+    LANGUAGE plpgsql
+AS
+$$
 BEGIN
-  EXECUTE format('COPY %I FROM %L WITH CSV HEADER DELIMITER %L',
-                substring(filename from '[^.]+'), 
-                filename,
-                delimiter);  
+    EXECUTE format('COPY %I FROM %L WITH CSV HEADER DELIMITER %L',
+                   substring(filename from '[^.]+'),
+                   filename,
+                   delimiter);
 END;
 $$;
 
 CREATE OR REPLACE FUNCTION export_to_csv(
     filename text,
-    delimiter char DEFAULT ','  
-) 
-RETURNS void
-LANGUAGE plpgsql 
-AS $$
+    delimiter char DEFAULT ','
+)
+    RETURNS void
+    LANGUAGE plpgsql
+AS
+$$
 BEGIN
-  EXECUTE format(
-    'COPY %I TO %L WITH CSV HEADER DELIMITER %L',
-    substring(filename from '[^.]+'), 
-    filename, 
-    delimiter
-  );
-END;  
+    EXECUTE format(
+            'COPY %I TO %L WITH CSV HEADER DELIMITER %L',
+            substring(filename from '[^.]+'),
+            filename,
+            delimiter
+        );
+END;
 $$;
 
 -- Использование:
